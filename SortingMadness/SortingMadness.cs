852b29c4f0bc683d0f91c13970fc7604c09afcff
Bill Nicholson <nicholdw@ucmail.uc.edu>
2017-09-26 11:50:52 -0400

    First Commit

---
+++ SortingMadness/SortingMadness.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortingMadness
+{
+    class SortingMadness
+    {
+        static void Main(string[] args)
+        {
+            SortingMadness sm = new SortingMadness();
+            sm.BubbleSort(sm.ReadTestData("testData.txt"));
+        }
+        public void BubbleSort(List<String> data)
+        {
+
+        }
+        public List<String> ReadTestData(String fileName)
+        {
+            List<String> testData = new List<String>();
+
+            return testData;
+        }
+    }
+}
